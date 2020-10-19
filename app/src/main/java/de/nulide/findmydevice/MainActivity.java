60ca2563a0a673c39cdadaebc2a3339ea33492a8
Nulide <Nulide@protonmail.com>
2020-10-19 16:36:05 +0200

    Added possibility to get gps and gsm-cell-tower location.

--- app/src/main/java/de/nulide/findmydevice/MainActivity.java
+++ app/src/main/java/de/nulide/findmydevice/MainActivity.java
@@ -2,19 +2,42 @@ package de.nulide.findmydevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.IntentFilter;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-import de.nulide.findmydevice.receiver.SMSReceiver;
+import android.os.Looper;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+import de.nulide.findmydevice.utils.GPS;
+import de.nulide.findmydevice.utils.SMS;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SMSReceiver rec = new SMSReceiver();
-        registerReceiver(rec, new IntentFilter(SMSReceiver.SMS_RECEIVED));
+        SMS rec = new SMS();
+        registerReceiver(rec, new IntentFilter(SMS.SMS_RECEIVED));
+
+        Log.d("SMS", "SMS ready");
+
+        GPS gps = new GPS(this);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Looper m = Looper.myLooper();
+        lm.requestSingleUpdate(LocationManager.GPS_PROVIDER, gps, m);
+        lm.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, gps, m);
+        Log.d("GPS", String.valueOf(gps.getLastBestLocation()));
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        GsmCellLocation location = (GsmCellLocation) tm.getCellLocation();
+        String networkOperator = tm.getNetworkOperator();
+        Log.d("gsm", location.getCid() + "/"+ location.getLac());
+        Log.d("gsm-2", networkOperator.substring(0,3)+ "/"+networkOperator.substring(3));
     }
 }
---
+++ app/src/main/java/de/nulide/findmydevice/utils/GPS.java
@@ -0,0 +1,118 @@
+package de.nulide.findmydevice.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+
+public class GPS implements LocationListener {
+
+    static final int WAIT_TIME = 1000 * 5;
+    private Location currentBestLocation = null;
+    private Context context;
+    private LocationManager locationManager;
+
+    @SuppressLint("MissingPermission")
+    public GPS(Context context){
+        this.context = context;
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (isBetterLocation(location, currentBestLocation)) {
+            currentBestLocation = location;
+        }
+        if (currentBestLocation == null) {
+            currentBestLocation = location;
+        }
+        String Latitude = getLastBestLocation().getLatitude()+ "";
+        String Longitude = getLastBestLocation().getLongitude() + "";
+        Log.d("GPS", Latitude + "/"+ Longitude);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public Location getLastBestLocation() {
+        @SuppressLint("MissingPermission") Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        @SuppressLint("MissingPermission") Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) {
+            GPSLocationTime = locationGPS.getTime();
+        }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if (0 < GPSLocationTime - NetLocationTime) {
+            return locationGPS;
+        } else {
+            return locationNet;
+        }
+    }
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            return true;
+        }
+
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > WAIT_TIME;
+        boolean isSignificantlyOlder = timeDelta < -WAIT_TIME;
+        boolean isNewer = timeDelta > 0;
+
+        if (isSignificantlyNewer) {
+            return true;
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        String provider1 = location.getProvider();
+        String provider2 = currentBestLocation.getProvider();
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        boolean isFromSameProvider = provider1.equals(provider2);
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else return isNewer && !isSignificantlyLessAccurate && isFromSameProvider;
+    }
+
+    public GsmCellLocation getGSMCellLocation(){
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        @SuppressLint("MissingPermission") GsmCellLocation location = (GsmCellLocation) tm.getCellLocation();
+        return location;
+    }
+}
