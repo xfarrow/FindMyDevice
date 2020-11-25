20bb1a1705f12506bf1496a29fabcb8c8ed870bb
Nulide <Nulide@protonmail.com>
2020-11-25 15:31:39 +0100

    FixedBug causing multiple replys to one request.

--- app/src/main/java/de/nulide/findmydevice/receiver/SMSReceiver.java
+++ app/src/main/java/de/nulide/findmydevice/receiver/SMSReceiver.java
@@ -8,6 +8,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import de.nulide.findmydevice.data.WhiteList;
 import de.nulide.findmydevice.data.io.IO;
 import de.nulide.findmydevice.utils.MessageHandler;
@@ -17,37 +20,44 @@ public class SMSReceiver extends BroadcastReceiver {
     public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
     private final WhiteList whitelist;
+    private Date timeUntilNextUsage;
 
     public SMSReceiver() {
         whitelist = IO.readWhiteList();
+        timeUntilNextUsage = Calendar.getInstance().getTime();
     }
 
     @SuppressLint("NewApi")
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        SmsMessage[] msgs;
-        String format = bundle.getString("format");
-        Object[] pdus = (Object[]) bundle.get("pdus");
-        if (pdus != null) {
-            boolean isVersionM =
-                    (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
-            msgs = new SmsMessage[pdus.length];
-            for (int i = 0; i < msgs.length; i++) {
-                if (isVersionM) {
-                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
-                } else {
-                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                }
-                String receiver = msgs[i].getOriginatingAddress();
-                for (int iwl = 0; iwl < whitelist.size(); iwl++) {
-                    if (receiver.equals(whitelist.get(iwl).getNumber())) {
-                        MessageHandler.handle(msgs[i].getOriginatingAddress(), msgs[i].getMessageBody(), context);
+        Date time = Calendar.getInstance().getTime();
+        if (time.getTime() > timeUntilNextUsage.getTime()) {
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs;
+            String format = bundle.getString("format");
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus != null) {
+                boolean isVersionM =
+                        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+                msgs = new SmsMessage[pdus.length];
+                for (int i = 0; i < msgs.length; i++) {
+                    if (isVersionM) {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                    } else {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    }
+                    String receiver = msgs[i].getOriginatingAddress();
+                    for (int iwl = 0; iwl < whitelist.size(); iwl++) {
+                        if (receiver.equals(whitelist.get(iwl).getNumber())) {
+                            MessageHandler.handle(msgs[i].getOriginatingAddress(), msgs[i].getMessageBody(), context);
+                        }
                     }
                 }
             }
+            Calendar now = Calendar.getInstance();
+            now.add(Calendar.SECOND, 2);
+            timeUntilNextUsage = now.getTime();
         }
-
     }
 
 }
