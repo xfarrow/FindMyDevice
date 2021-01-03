1c05fa7e2d07e19095c6c52a5c40cb65ef7babf7
Nulide <Nulide@protonmail.com>
2021-01-03 21:45:45 +0100

    Fixed Bug: Reply won't get delivered.

--- app/src/main/java/de/nulide/findmydevice/utils/SMS.java
+++ app/src/main/java/de/nulide/findmydevice/utils/SMS.java
@@ -2,12 +2,19 @@ package de.nulide.findmydevice.utils;
 
 import android.telephony.SmsManager;
 
+import java.util.ArrayList;
+
 public class SMS {
 
     public static void sendMessage(String destination, String msg) {
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage
-                (destination, null, msg,
-                        null, null);
+        if(msg.length() <= 160){
+            smsManager.sendTextMessage
+                    (destination, null, msg,
+                            null, null);
+        }else {
+            ArrayList<String> parts = smsManager.divideMessage(msg);
+            smsManager.sendMultipartTextMessage(destination, null, parts, null, null);
+        }
     }
 }
