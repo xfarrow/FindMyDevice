b4d6e8f052ab6f914a3135b6fd3f3e2bb3e79c55
Nulide <Nulide@protonmail.com>
2021-02-07 20:47:27 +0100

    Improved MessageHandler

--- app/src/main/java/de/nulide/findmydevice/logic/MessageHandler.java
+++ app/src/main/java/de/nulide/findmydevice/logic/MessageHandler.java
@@ -22,6 +22,12 @@ import de.nulide.findmydevice.utils.SMS;
 
 public class MessageHandler {
 
+    private static String COM_LOCATE = "locate";
+    private static String COM_RING = "ring";
+    private static String COM_LOCK = "lock";
+    private static String COM_DELETE = "delete";
+    private static String COM_STATS = "stats";
+
     private static int counter = 0;
     private static Settings settings;
 
@@ -33,82 +39,86 @@ public class MessageHandler {
         String originalMsg = msg;
         msg = msg.toLowerCase();
         StringBuilder replyBuilder = new StringBuilder();
-        if (msg.startsWith(settings.get(Settings.SET_FMD_COMMAND) + " locate") && Permission.GPS) {
-            if (Permission.WRITE_SECURE_SETTINGS) {
-                if (!GPS.isGPSOn(context)) {
-                    GPS.turnOnGPS(context);
-                }
-            }
-            replyBuilder.append(context.getString(R.string.MH_GPS_WILL_FOLLOW));
-            GPS gps = new GPS(context, sender);
-            gps.sendGSMCellLocation(settings);
-        } else if (msg.startsWith(settings.get(Settings.SET_FMD_COMMAND) + " ring")) {
-            replyBuilder.append(context.getString(R.string.MH_rings));
-            if (msg.contains("long")) {
-                Ringer.ring(context, 180);
-            } else {
-                Ringer.ring(context, 15);
-            }
-        } else if (msg.startsWith(settings.get(Settings.SET_FMD_COMMAND) + " lock") && Permission.DEVICE_ADMIN) {
-            DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-            devicePolicyManager.lockNow();
-            Intent lockScreenMessage = new Intent(context, LockScreenMessage.class);
-            lockScreenMessage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            lockScreenMessage.putExtra(LockScreenMessage.SENDER, sender);
-            if (msg.length() > ((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 6) {
-                String customMessage = originalMsg.substring(((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 6, msg.length());
-                lockScreenMessage.putExtra(LockScreenMessage.CUSTOM_TEXT, customMessage);
-            }
-            context.startActivity(lockScreenMessage);
-            replyBuilder.append(context.getString(R.string.MH_Locked));
-        } else if (msg.startsWith(settings.get(Settings.SET_FMD_COMMAND) + " stats")) {
-            replyBuilder.append(context.getString(R.string.MH_Stats));
-            Map<String, String> ips = Network.getAllIP();
-            Iterator<String> it = ips.keySet().iterator();
-            while (it.hasNext()) {
-                String intf = it.next();
-                replyBuilder.append(intf).append(": ").append(ips.get(intf)).append("\n");
-            }
-            replyBuilder.append(context.getString(R.string.MH_Networks));
-            for (ScanResult sr : Network.getWifiNetworks(context)) {
-                replyBuilder.append(sr.SSID).append("\n");
-            }
-        } else if (msg.startsWith(settings.get(Settings.SET_FMD_COMMAND) + " delete") && Permission.DEVICE_ADMIN) {
-            if ((Boolean) settings.get(Settings.SET_WIPE_ENABLED)) {
-                DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-                if (msg.length() > ((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 8) {
-                    String pin = originalMsg.substring(((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 8, msg.length());
-                    if (BCryptUtils.checkPasswordHash((String) settings.get(Settings.SET_PIN), pin)) {
-                        devicePolicyManager.wipeData(0);
-                        replyBuilder.append(context.getString(R.string.MH_Delete));
-                    } else {
-                        replyBuilder.append(context.getString(R.string.MH_False_Pin));
+        if(msg.startsWith((String)settings.get(Settings.SET_FMD_COMMAND))) {
+            originalMsg = originalMsg.substring(((String) settings.get(Settings.SET_FMD_COMMAND)).length()+1, msg.length());
+            msg = msg.substring(((String) settings.get(Settings.SET_FMD_COMMAND)).length()+1, msg.length());
+            if (msg.startsWith(COM_LOCATE) && Permission.GPS) {
+                if (Permission.WRITE_SECURE_SETTINGS) {
+                    if (!GPS.isGPSOn(context)) {
+                        GPS.turnOnGPS(context);
                     }
+                }
+                replyBuilder.append(context.getString(R.string.MH_GPS_WILL_FOLLOW));
+                GPS gps = new GPS(context, sender);
+                gps.sendGSMCellLocation(settings);
+            } else if (msg.startsWith(COM_RING)) {
+                replyBuilder.append(context.getString(R.string.MH_rings));
+                if (msg.contains("long")) {
+                    Ringer.ring(context, 180);
                 } else {
-                    replyBuilder.append(context.getString(R.string.MH_Syntax)).append((String) settings.get(Settings.SET_FMD_COMMAND)).append(" delete [pin]");
+                    Ringer.ring(context, 15);
+                }
+            } else if (msg.startsWith(COM_LOCK) && Permission.DEVICE_ADMIN) {
+                DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                devicePolicyManager.lockNow();
+                Intent lockScreenMessage = new Intent(context, LockScreenMessage.class);
+                lockScreenMessage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                lockScreenMessage.putExtra(LockScreenMessage.SENDER, sender);
+                if (msg.length() > ((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 6) {
+                    String customMessage = originalMsg.substring(((String) settings.get(Settings.SET_FMD_COMMAND)).length() + 6, msg.length());
+                    lockScreenMessage.putExtra(LockScreenMessage.CUSTOM_TEXT, customMessage);
+                }
+                context.startActivity(lockScreenMessage);
+                replyBuilder.append(context.getString(R.string.MH_Locked));
+            } else if (msg.startsWith(COM_STATS)) {
+                replyBuilder.append(context.getString(R.string.MH_Stats));
+                Map<String, String> ips = Network.getAllIP();
+                Iterator<String> it = ips.keySet().iterator();
+                while (it.hasNext()) {
+                    String intf = it.next();
+                    replyBuilder.append(intf).append(": ").append(ips.get(intf)).append("\n");
+                }
+                replyBuilder.append(context.getString(R.string.MH_Networks));
+                for (ScanResult sr : Network.getWifiNetworks(context)) {
+                    replyBuilder.append(sr.SSID).append("\n");
+                }
+            } else if (msg.startsWith(COM_DELETE) && Permission.DEVICE_ADMIN) {
+                if ((Boolean) settings.get(Settings.SET_WIPE_ENABLED)) {
+                    DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                    if (msg.length() > COM_DELETE.length()+1) {
+                        String pin = originalMsg.substring(COM_DELETE.length()+1, msg.length());
+                        if (BCryptUtils.checkPasswordHash((String) settings.get(Settings.SET_PIN), pin)) {
+                            devicePolicyManager.wipeData(0);
+                            replyBuilder.append(context.getString(R.string.MH_Delete));
+                        } else {
+                            replyBuilder.append(context.getString(R.string.MH_False_Pin));
+                        }
+                    } else {
+                        replyBuilder.append(context.getString(R.string.MH_Syntax)).append((String) settings.get(Settings.SET_FMD_COMMAND)).append(" delete [pin]");
+                    }
+                }
+            } else {
+                replyBuilder.append(context.getString(R.string.MH_Title_Help)).append("\n");
+                if (Permission.GPS) {
+                    replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_where)).append("\n");
+                }
+                replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_ring)).append("\n");
+                if (Permission.DEVICE_ADMIN) {
+                    replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_Lock)).append("\n");
+                }
+                replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_Stats));
+                if ((Boolean) settings.get(Settings.SET_WIPE_ENABLED)) {
+                    replyBuilder.append("\n").append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_delete));
                 }
             }
-        } else if (msg.startsWith((String) settings.get(Settings.SET_FMD_COMMAND))) {
-            replyBuilder.append(context.getString(R.string.MH_Title_Help)).append("\n");
-            if (Permission.GPS) {
-                replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_where)).append("\n");
-            }
-            replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_ring)).append("\n");
-            if (Permission.DEVICE_ADMIN) {
-                replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_Lock)).append("\n");
-            }
-            replyBuilder.append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_Stats));
-            if ((Boolean) settings.get(Settings.SET_WIPE_ENABLED)) {
-                replyBuilder.append("\n").append((String) settings.get(Settings.SET_FMD_COMMAND)).append(context.getString(R.string.MH_Help_delete));
-            }
-        }
 
-        String reply = replyBuilder.toString();
-        if (!reply.isEmpty()) {
-            Logger.log("Command used", msg);
-            counter++;
-            Notifications.notify(context, "SMS-Receiver", "New Usage " + counter, Notifications.CHANNEL_USAGE);
-            SMS.sendMessage(sender, reply);
+            String reply = replyBuilder.toString();
+            if (!reply.isEmpty()) {
+                Logger.log("Command used", msg);
+                counter++;
+                Notifications.notify(context, "SMS-Receiver", "New Usage " + counter, Notifications.CHANNEL_USAGE);
+                SMS.sendMessage(sender, reply);
+            }
         }
     }
 
