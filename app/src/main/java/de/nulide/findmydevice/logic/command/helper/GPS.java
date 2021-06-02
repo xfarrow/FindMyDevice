23fe1966fb3ff5d080cc1a2c3b82c25ab82b5102
Nulide <Nulide@tutanota.de>
2021-06-02 16:15:02 +0200

    Optimize locate logic.

--- app/src/main/java/de/nulide/findmydevice/logic/command/helper/GPS.java
+++ app/src/main/java/de/nulide/findmydevice/logic/command/helper/GPS.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -23,6 +24,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import de.nulide.findmydevice.R;
 import de.nulide.findmydevice.data.Settings;
 import de.nulide.findmydevice.logic.ComponentHandler;
@@ -31,18 +35,31 @@ import de.nulide.findmydevice.utils.SecureSettings;
 public class GPS implements LocationListener {
 
     private ComponentHandler ch;
-    private final LocationManager locationManager;
-    private int providerSize = 0;
-    private int providerIndex = 0;
+    private LocationManager locationManager;
+    private int sizeToReach = 0;
+    private List<GPS> listeners;
+    private String provider;
+    private GPS listenerHandler;
 
     @SuppressLint("MissingPermission")
     public GPS(ComponentHandler ch) {
         this.ch = ch;
         locationManager = (LocationManager) ch.getContext().getSystemService(Context.LOCATION_SERVICE);
+        listeners = new LinkedList<>();
         for (String provider : locationManager.getAllProviders()) {
-            providerSize++;
-            locationManager.requestLocationUpdates(provider, 0, 0, this);
+            GPS listener = new GPS(ch, provider, this);
+            listeners.add(listener);
+            locationManager.requestLocationUpdates(provider, 0, 0, listener);
         }
+        sizeToReach = listeners.size()-1;
+    }
+
+    public GPS(ComponentHandler ch, String provider, GPS boss){
+        this.ch = ch;
+        locationManager = (LocationManager) ch.getContext().getSystemService(Context.LOCATION_SERVICE);
+        this.provider = provider;
+        listeners = new LinkedList<>();
+        listenerHandler = boss;
     }
 
     public static boolean isGPSOn(Context context) {
@@ -64,11 +81,24 @@ public class GPS implements LocationListener {
             String provider = location.getProvider();
             String lat = new Double(location.getLatitude()).toString();
             String lon = new Double(location.getLongitude()).toString();
-            providerIndex++;
             ch.getLocationHandler().newLocation(provider, lat, lon);
-            if (providerSize <= providerIndex && ((Integer) ch.getSettings().get(Settings.SET_GPS_STATE_BEFORE) == 0)) {
-                SecureSettings.turnGPS(ch.getContext(), false);
-                ch.getSettings().set(Settings.SET_GPS_STATE_BEFORE, 1);
+            locationManager.removeUpdates(this);
+            listenerHandler.onListenerDone(provider);
+        }
+    }
+
+    public void onListenerDone(String provider){
+        for(GPS listener: listeners){
+            if(listener.getProvider().equals(provider)){
+                listeners.remove(listener);
+                if (sizeToReach >= listeners.size() && ((Integer) ch.getSettings().get(Settings.SET_GPS_STATE_BEFORE) == 0)) {
+                    SecureSettings.turnGPS(ch.getContext(), false);
+                    ch.getSettings().set(Settings.SET_GPS_STATE_BEFORE, 1);
+                    for(GPS toRemove: listeners){
+                        locationManager.removeUpdates(toRemove);
+                    }
+                }
+                return;
             }
         }
     }
@@ -85,7 +115,11 @@ public class GPS implements LocationListener {
 
     @Override
     public void onProviderDisabled(@NonNull String provider) {
-        locationManager.removeUpdates(this);
+        for(GPS listener: listeners){
+            if(listener.getProvider().equals(provider)){
+                locationManager.removeUpdates(listener);
+            }
+        }
     }
 
     public GsmCellLocation sendGSMCellLocation() {
@@ -143,5 +177,9 @@ public class GPS implements LocationListener {
                 }
             }
         }
+    }
+
+    public String getProvider(){
+        return provider;
     }
 }
