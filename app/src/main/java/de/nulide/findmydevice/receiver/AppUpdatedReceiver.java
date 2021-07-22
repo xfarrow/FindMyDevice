451011367947463518574bdeedbe2fb91aedd4f0
Nulide <Nulide@tutanota.de>
2021-07-22 22:29:30 +0200

    Only registered user will be asked to reregister

--- app/src/main/java/de/nulide/findmydevice/receiver/AppUpdatedReceiver.java
+++ app/src/main/java/de/nulide/findmydevice/receiver/AppUpdatedReceiver.java
@@ -26,11 +26,13 @@ public class AppUpdatedReceiver extends SuperReceiver {
             config.set(ConfigSMSRec.CONF_TEMP_WHITELISTED_CONTACT_ACTIVE_SINCE, null);
 
             //One Time due to vulnerability
-            ch.getSettings().set(Settings.SET_FMDSERVER_ID, "");
-            ch.getSettings().set(Settings.SET_FMDSERVER_PASSWORD_SET, false);
-            ch.getSettings().set(Settings.SET_FMDSERVER, false);
-            Notifications.notify(context, "FMDServer", "You need to register to the server to use it.", Notifications.CHANNEL_LIFE);
-            Toast.makeText(context, "Please reregister to FMDServer", Toast.LENGTH_LONG).show();
+            if(!((String)ch.getSettings().get(Settings.SET_FMDSERVER_ID)).isEmpty()) {
+                ch.getSettings().set(Settings.SET_FMDSERVER_ID, "");
+                ch.getSettings().set(Settings.SET_FMDSERVER_PASSWORD_SET, false);
+                ch.getSettings().set(Settings.SET_FMDSERVER, false);
+                Notifications.notify(context, "FMDServer", "You need to register to the server to use it.", Notifications.CHANNEL_LIFE);
+                Toast.makeText(context, "Please reregister to FMDServer", Toast.LENGTH_LONG).show();
+            }
 
             if((Boolean)ch.getSettings().get(Settings.SET_FMDSERVER)){
                 FMDServerService.scheduleJob(context, 0);
