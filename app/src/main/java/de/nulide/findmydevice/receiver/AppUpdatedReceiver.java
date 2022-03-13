ba1036c57d68de30bceb37dd08e97fe566ab5361
Nulide <Nulide@tutanota.de>
2022-03-13 15:15:27 +0100

    BugFix: reregistration on all devices that update fmd.

--- app/src/main/java/de/nulide/findmydevice/receiver/AppUpdatedReceiver.java
+++ app/src/main/java/de/nulide/findmydevice/receiver/AppUpdatedReceiver.java
@@ -29,15 +29,6 @@ public class AppUpdatedReceiver extends SuperReceiver {
             config.set(ConfigSMSRec.CONF_TEMP_WHITELISTED_CONTACT, null);
             config.set(ConfigSMSRec.CONF_TEMP_WHITELISTED_CONTACT_ACTIVE_SINCE, null);
 
-            //One Time due to vulnerability
-            if(!((String)ch.getSettings().get(Settings.SET_FMDSERVER_ID)).isEmpty()) {
-                ch.getSettings().set(Settings.SET_FMDSERVER_ID, "");
-                ch.getSettings().set(Settings.SET_FMDSERVER_PASSWORD_SET, false);
-                ch.getSettings().set(Settings.SET_FMDSERVER_UPLOAD_SERVICE, false);
-                Notifications.notify(context, "FMDServer", "You need to register to the server to use it.", Notifications.CHANNEL_LIFE);
-                Toast.makeText(context, "Please reregister to FMDServer", Toast.LENGTH_LONG).show();
-            }
-
             if((Boolean)ch.getSettings().get(Settings.SET_FMDSERVER_UPLOAD_SERVICE)){
                 FMDServerService.scheduleJob(context, 0);
 
