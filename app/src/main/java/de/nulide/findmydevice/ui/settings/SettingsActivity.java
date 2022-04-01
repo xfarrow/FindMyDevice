9b1e476f1611910fb1572c196d1f0ae41f8cad96
Nulide <Nulide@tutanota.de>
2022-04-01 21:41:01 +0200

    Add possibility to export settings.

--- app/src/main/java/de/nulide/findmydevice/ui/settings/SettingsActivity.java
+++ app/src/main/java/de/nulide/findmydevice/ui/settings/SettingsActivity.java
@@ -1,17 +1,36 @@
 package de.nulide.findmydevice.ui.settings;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.nulide.findmydevice.R;
+import de.nulide.findmydevice.data.Settings;
+import de.nulide.findmydevice.data.io.IO;
+import de.nulide.findmydevice.data.io.JSONFactory;
+import de.nulide.findmydevice.data.io.json.JSONMap;
 import de.nulide.findmydevice.ui.IntroductionActivity;
 import de.nulide.findmydevice.ui.LogActivity;
 import de.nulide.findmydevice.ui.helper.SettingsViewAdapter;
@@ -21,6 +40,10 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
     private ListView listSettings;
 
     private List<String> settingsEntries;
+
+    private final int EXPORT_REQ_CODE = 30;
+
+    private final int IMPORT_REQ_CODE = 40;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +56,8 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         settingsEntries.add(getString(R.string.Settings_WhiteList));
         settingsEntries.add(getString(R.string.Settings_OpenCellId));
         settingsEntries.add(getString(R.string.Settings_Permissions));
+        settingsEntries.add(getString(R.string.Settings_Export));
+        settingsEntries.add(getString(R.string.Settings_Import));
         settingsEntries.add(getString(R.string.Settings_Logs));
         settingsEntries.add(getString(R.string.Settings_About));
 
@@ -66,15 +91,91 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
                 break;
 
             case 5:
-                settingIntent = new Intent(this, LogActivity.class);
+                settingIntent = null;
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.putExtra(Intent.EXTRA_TITLE, IO.settingsFileName);
+                intent.setType("*/*");
+                startActivityForResult(intent, EXPORT_REQ_CODE);
                 break;
 
             case 6:
+                settingIntent = null;
+                intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, IMPORT_REQ_CODE);
+
+                break;
+
+            case 7:
+                settingIntent = new Intent(this, LogActivity.class);
+                break;
+
+            case 8:
                 settingIntent = new Intent(this, AboutActivity.class);
                 break;
         }
         if(settingIntent != null){
             startActivity(settingIntent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMPORT_REQ_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(uri);
+
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder json = new StringBuilder();
+                    try {
+                        String line;
+
+                        while ((line = br.readLine()) != null) {
+                            json.append(line);
+                            json.append('\n');
+                        }
+                        br.close();
+                        String text = json.toString();
+                        ObjectMapper mapper = new ObjectMapper();
+                        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                        if (!text.isEmpty()) {
+                            Settings settings = mapper.readValue(text, Settings.class);
+                            settings.setNow(Settings.SET_INTRODUCTION_VERSION, settings.get(Settings.SET_INTRODUCTION_VERSION));
+                            finish();
+                            startActivity(getIntent());
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (requestCode == EXPORT_REQ_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                try {
+                    ParcelFileDescriptor sco = this.getContentResolver().openFileDescriptor(uri, "w");
+                    PrintWriter out = new PrintWriter(new FileOutputStream(sco.getFileDescriptor()));
+                    Settings settings = JSONFactory.convertJSONSettings(IO.read(JSONMap.class, IO.settingsFileName));
+                    ObjectMapper mapper = new ObjectMapper();
+                    String json = mapper.writeValueAsString(settings);
+                    out.write(json);
+                    out.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
